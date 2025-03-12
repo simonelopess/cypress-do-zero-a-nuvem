20bae106b1668e8425dd1112b9dd8527c6954faa
simone <simone.l.silva@parceiros.nos.pt>
2025-03-12 13:19:13 +0000

    chore: add custom tests and common tests

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,53 +1,84 @@
 describe('Central de Atendimento ao Cliente TAT', () => {
-  const localServer = 'src/index.html';
 
-
-  const fullNameInput = 'input[name="firstName"]';
-  const lastNameInput = 'input[name="lastName"]';
-  const emailInput = 'input[name="email"]#email';
-  const phoneInput = 'input[name="phone"]#phone';
-  const messageInput = 'textarea[name="message"]';
-  const submitButton = 'button[type="submit"]';
-
+  beforeEach(() => {
+    cy.visit(Cypress.env('host'))
+  }) 
 
   it('verifica o título da aplicação', () => {
-    cy.visit(localServer)
-    cy.title('h1').should('contain', 'Central de Atendimento ao Cliente TAT')
+    cy.title().should('contain', 'Central de Atendimento ao Cliente TAT')
   })
 
-  it('verifica se o campo de busca está visível', () => {
-    cy.visit(localServer)
-    cy.get(fullNameInput).should('be.visible')
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Simone')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('teste@gmail.com')
+    cy.get('#open-text-area').type('obrigada')
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.success').should('be.visible')
   })
 
-  it('verifica se o campo "nome" foi digitado corretamente', () => {
-    cy.visit(localServer)
-    cy.get(fullNameInput)
-    .type('Teste de campo de texto')
-    .should('have.value', 'Teste de campo de texto')
+  /* Exercício 1 */
+  it('teste campo de texto longo com delay', () => {
+    const longText = Cypress._.repeat('a1231232132132132132132112312312312321', 10) // repete uma string varias vezes
+
+    cy.get('#open-text-area').type(longText, { delay: 0 })
   })
 
-  it('verifica se o campo de "sobrenome" foi digitado corretamente', () => {
-    cy.visit(localServer)
-    cy.get(lastNameInput)
-    .type('Teste de campo lastName')
-    .should('have.value', 'Teste de campo lastName')
+  /* Exercício 2 */
+  it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', () => {
+    cy.get('#firstName').type('Simone')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('teste')
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+
   })
 
-  it('verifica se o campo de "email" foi digitado corretamente', () => {
-    cy.visit(localServer)
-    cy.get(emailInput)
-    .type('teste@gmail.com')
-    .should('have.value', 'teste@gmail.com')
+  /* Exercício 3 */
+  it('campo telefone continua vazio quando preenchido com valor não númerico', () => {
+    cy.get('#phone')
+      .type('abcde')
+      .should('have.value', '')
   })
 
-  it('verifica se o campo de "telefone" foi digitado corretamente', () => {
-    cy.visit(localServer)
-    cy.get(phoneInput)
-    .type('99999999')
+  /* Exercício 4 */
+  it('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', () => {
+    cy.get('#firstName').type('Simone')
+    cy.get('#lastName').type('Silva')
+    cy.get('#email').type('teste@teste.com')
+
+    cy.get('#phone-checkbox').click()
+
+    cy.contains('button', 'Enviar').click()
+
+    cy.get('.error').should('be.visible')
+
+  })
+  
+  /* Exercício 5 */ 
+  it('exibe mensagem de erro ao submeter o formulário com um email vazio', () => {
+
+    cy.get('#lastName')
+      .type('Hello')
+      .should('have.value', 'Hello')
+      .clear()
+      .should('have.value', '')
     
-    cy.get(phoneInput).should('have.value', '99999999')
+  })
+
+  /* Exercício 6 */
+  it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', () => {
+    cy.contains('button', 'Enviar').click()
+    cy.get('.error').should('be.visible')
   })
 
 
+  /* COMANDO CUSTOMIZADO */
+  it('envia o formuário com sucesso usando um comando customizado', () =>{
+    cy.fillMandatoryFieldsAndSubmit()
+
+    cy.get('.success').should('be.visible')
+  })
 })
